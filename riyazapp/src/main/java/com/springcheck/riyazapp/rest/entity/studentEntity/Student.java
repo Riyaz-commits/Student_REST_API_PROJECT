94c072c06a1eb7a071f7b150a16b565c82b77248
Riyaz-commits <skriyazbasha91@gmail.com>
2023-10-29 13:31:29 +0530

    changes with lambok

--- riyazapp/src/main/java/com/springcheck/riyazapp/rest/entity/studentEntity/Student.java
+++ riyazapp/src/main/java/com/springcheck/riyazapp/rest/entity/studentEntity/Student.java
@@ -1,74 +1,28 @@
 package com.springcheck.riyazapp.rest.entity.studentEntity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
-@Table(name="student")
+@Table(name = "student")
+@Data
 public class Student {
 
-    public Integer getRollNo() {
-        return rollNo;
-    }
 
-
-    public void setRollNo(int rollNo) {
-        this.rollNo = rollNo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    @Column(name="rollno",nullable=true)
-    private Integer rollNo;
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "rollNo=" + rollNo +'\'' +
-        "id=" + id +
-                ", name='" + name + '\'' +
-                ", grade='" + grade + '\'' +
-                '}';
-    }
-
-    @Column(name="name",nullable=true)
+    @Column(name = "name", nullable = true)
     private String name;
 
     @Id
-  //  @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id",nullable=true)
+    //  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = true)
     private Integer id;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Column(name="grade",nullable=true)
+    @Column(name = "grade", nullable = true)
     private String grade;
-    public Student(Integer rollNo, Integer id, String name, String grade) {
-        this.rollNo = rollNo;
-        this.name = name;
-        this.id = id;
-        this.grade = grade;
-    }
 
-    public Student(){}
+    @Column(name = "rollno", nullable = true)
+    private Integer rollNo;
+    public Student() {
+    }
 
 }
